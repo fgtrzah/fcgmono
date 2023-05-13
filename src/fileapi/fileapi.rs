11c39f28e662e1049cba34c760addfade7cb3c0e
figtreez <figtreez@icloud.com>
2023-05-12 23:17:56 -0700

    init depth 1 req - pin file api marshalling

---
+++ src/fileapi/fileapi.rs
@@ -0,0 +1,104 @@
+// Example code that deserializes and serializes the model.
+// extern crate serde;
+// #[macro_use]
+// extern crate serde_derive;
+// extern crate serde_json;
+//
+// use generated_module::File;
+//
+// fn main() {
+//     let json = r#"{"answer": 42}"#;
+//     let model: File = serde_json::from_str(&json).unwrap();
+// }
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct File {
+    pub(crate) document: Option<Document>,
+    pub(crate) components: Option<ComponentSets>,
+    #[serde(rename = "componentSets")]
+    pub(crate) component_sets: Option<ComponentSets>,
+    #[serde(rename = "schemaVersion")]
+    pub(crate) schema_version: Option<i64>,
+    pub(crate) styles: Option<ComponentSets>,
+    pub(crate) name: Option<String>,
+    #[serde(rename = "lastModified")]
+    pub(crate) last_modified: Option<String>,
+    #[serde(rename = "thumbnailUrl")]
+    pub(crate) thumbnail_url: Option<String>,
+    pub(crate) version: Option<String>,
+    pub(crate) role: Option<String>,
+    #[serde(rename = "editorType")]
+    pub(crate) editor_type: Option<String>,
+    #[serde(rename = "linkAccess")]
+    pub(crate) link_access: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ComponentSets {
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Document {
+    pub(crate) id: Option<String>,
+    pub(crate) name: Option<String>,
+    #[serde(rename = "type")]
+    pub(crate) document_type: Option<String>,
+    #[serde(rename = "scrollBehavior")]
+    pub(crate) scroll_behavior: Option<ScrollBehavior>,
+    pub(crate) children: Option<Vec<Child>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Child {
+    pub(crate) id: Option<String>,
+    pub(crate) name: Option<String>,
+    #[serde(rename = "type")]
+    pub(crate) child_type: Option<Type>,
+    #[serde(rename = "scrollBehavior")]
+    pub(crate) scroll_behavior: Option<ScrollBehavior>,
+    pub(crate) children: Option<Vec<Option<serde_json::Value>>>,
+    #[serde(rename = "backgroundColor")]
+    pub(crate) background_color: Option<BackgroundColor>,
+    #[serde(rename = "prototypeStartNodeID")]
+    pub(crate) prototype_start_node_id: Option<serde_json::Value>,
+    #[serde(rename = "flowStartingPoints")]
+    pub(crate) flow_starting_points: Option<Vec<Option<serde_json::Value>>>,
+    #[serde(rename = "prototypeDevice")]
+    pub(crate) prototype_device: Option<PrototypeDevice>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BackgroundColor {
+    pub(crate) r: Option<f64>,
+    pub(crate) g: Option<f64>,
+    pub(crate) b: Option<f64>,
+    pub(crate) a: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PrototypeDevice {
+    #[serde(rename = "type")]
+    pub(crate) prototype_device_type: Option<Rotation>,
+    pub(crate) rotation: Option<Rotation>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Type {
+    #[serde(rename = "CANVAS")]
+    Canvas,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Rotation {
+    #[serde(rename = "NONE")]
+    None,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ScrollBehavior {
+    #[serde(rename = "SCROLLS")]
+    Scrolls,
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+pub mod fileapi;
+
+use fileapi::fileapi::File;
+use serde_json;
 use std::env;
 use reqwest; // 0.10.0
 use tokio; // 0.2.6
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
+
+fn inspect_file_struct(json: String) -> File {
+    let model: File = serde_json::from_str(&json).unwrap();
+
+    model
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -13,13 +23,11 @@ async fn main() -> Result<(), reqwest::Error> {
         Some(v) => v.into_string().unwrap(),
         None => panic!("$FILE is not set")
     };
-    println!("{}", file);
 
     let token = match env::var_os("FIGMA_TOKEN") {
         Some(v) => v.into_string().unwrap(),
         None => panic!("$FIGMA_TOKEN is not set")
     };
-    println!("{}", token);
     
     let res = client
         .get(format!("https://api.figma.com/v1/files/{}?depth=1", file))
@@ -27,7 +35,14 @@ async fn main() -> Result<(), reqwest::Error> {
         .send()
         .await?;
 
-    println!("{}", res.text().await?);
+    let text_body = res
+        .text()
+        .await?;
+    println!("{:?}", text_body);
+
+    let model = inspect_file_struct(text_body);
+
+    println!("{:?}", model.styles);
     
     Ok(())
 }
